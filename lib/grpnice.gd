f49da1a842a1617ea6bfe45186b5893652431f95
cdwensley <cdwensley.maths@btinternet.com>
2023-12-02 20:18:08 +0000

    fixing problems reported by LINT

--- lib/grpnice.gd
+++ lib/grpnice.gd
@@ -652,10 +652,10 @@ BindGlobal( "SubgroupMethodByNiceMonomorphismCollColl", function( oper, par )
                 TryNextMethod();
             fi;
             nice := NiceMonomorphism(obj1);
-	    img:=ImagesSet(nice,obj2);
-            if img = fail or 
-	      not (IsSubset(ImagesSource(nice),img) and
-		PreImagesSetNC(nice,img)=obj2) then
+            img:=ImagesSet(nice,obj2);
+            if img = fail
+              or not (IsSubset(ImagesSource(nice),img)
+                 and PreImagesSetNC(nice,img)=obj2) then
                  TryNextMethod();
             fi;
             img := oper( NiceObject(obj1), img );
